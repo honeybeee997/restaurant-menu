4e4156b551a874b41a0819d9818cd6bc878ecfd5
Muneeb <ahmedmuneeb997@gmail.com>
2024-09-23 21:59:47 +0500

    FEAT: Added 404

--- src/app/menu/[id]/page.js
+++ src/app/menu/[id]/page.js
@@ -1,12 +1,28 @@
+import Link from "next/link";
 import React from "react";
 import PageIntro from "~/components/layout/PageIntro";
 import Pricing from "~/components/pricing";
+import Button from "~/components/ui/button";
 
 import { MENU } from "~/constants";
 import { getMenuById } from "~/utils";
 
 const page = ({ params }) => {
   const menu = getMenuById(params.id);
+
+  if (!menu) {
+    return (
+      <div className="container flex flex-col items-center">
+        <h1 className="text-6xl text-center font-[circular]">404 !</h1>
+        <h4 className="text-xl text-center my-10 underline underline-offset-4">
+          لم يتم العثور عليه{" "}
+        </h4>
+        <Button className="!mt-0">
+          <Link href="/">العودة إلى القائمة</Link>
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <>
